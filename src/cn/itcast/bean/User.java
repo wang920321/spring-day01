d3b65c53339501d17f066bab3883673c0ac8f8f0
wang920321 <m13372782171_1@163.com>
2018-03-26 01:27:15 +0800

    资料

---
+++ src/cn/itcast/bean/User.java
@@ -0,0 +1,49 @@
+/**  
+
+* Title: User.java  
+
+* Description:  
+
+* Copyright: Copyright (c) 2017 
+
+* Company: www.baidudu.com 
+
+* @author 172219902  
+
+* @date 2018年3月26日  
+
+* @version 1.0  
+
+*/
+package cn.itcast.bean;
+
+
+public class User {
+    private String name;
+    private Integer age;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the age
+	 */
+	public Integer getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+    
+}
